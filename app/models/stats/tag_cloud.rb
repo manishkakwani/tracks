7f0667858af6fcaa73e1a8f7bad25b8af289b9c9
Manish Kakwani <mkakwani@teladoc.com>
2013-10-11 13:53:54 -0400

    iteration 9 with additional refactoring I did

--- app/models/stats/tag_cloud.rb
+++ app/models/stats/tag_cloud.rb
@@ -2,7 +2,7 @@ class TagCloud
   # tag cloud code inspired by this article
   # http://www.juixe.com/techknow/index.php/2006/07/15/acts-as-taggable-tag-cloud/
 
-	attr_reader :user, :tags, :min, :divisor,
+	attr_reader :user, :min, :divisor,
 							:for_cloud, :min, :divisor
 
 	def initialize(user, cut_off = nil)
@@ -10,17 +10,30 @@ class TagCloud
 		@cut_off = cut_off
 	end
 
-	def compute
-    levels=10
-    # TODO: parameterize limit
-    params = [sql(@cut_off), user.id]
-    if @cut_off
-    	params += [@cut_off, @cut_off]
-    end
-    @tags = Tag.find_by_sql(params).sort_by { |tag| tag.name.downcase }
+	def tags
+		#note: this is a further memoization refactoring I did
+		@tags ||= find_tags
+		#unless @tags
+	    #params = [sql(@cut_off), user.id]
+	    #if @cut_off
+	    	#params += [@cut_off, @cut_off]
+	    #end
+      #@tags = Tag.find_by_sql(params).sort_by { |tag| tag.name.downcase }
+    #end
+    #@tags
+  end
 
+  def find_tags
+		params = [sql(@cut_off), user.id]
+		if @cut_off
+		  params += [@cut_off, @cut_off]
+		end
+		Tag.find_by_sql(params).sort_by { |tag| tag.name.downcase }
+  end
+
+	def compute
     max, @min = 0, 0
-    @tags.each { |t|
+    tags.each { |t|
       max = [t.count.to_i, max].max
       @min = [t.count.to_i, @min].min
     }
@@ -43,6 +56,7 @@ class TagCloud
 	end
 
 	private
+    # TODO: parameterize limit
 	  def sql(cut_off = nil)
 	    query = "SELECT tags.id, tags.name AS name, count(*) AS count"
 	    query << " FROM taggings, tags, todos"
@@ -57,5 +71,9 @@ class TagCloud
 	    query << " GROUP BY tags.id, tags.name"
 	    query << " ORDER BY count DESC, name"
 	    query << " LIMIT 100"
+	  end
+
+	  def levels
+	  	10
 	  end
 end
